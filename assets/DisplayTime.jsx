a6739ba2fa8461e6b0e0d0f158fc948716fd8721
Curanda <103290697+Curanda@users.noreply.github.com>
2023-08-28 23:27:34 +0000

    Create DisplayTime.jsx

---
+++ assets/DisplayTime.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import dayjs from "dayjs";
+
+function DisplayTime({ count }) {
+    const now = dayjs();
+
+    if (count > 0) {
+        return <p>{count} days from today is {now.add(count, 'day').format('YYYY-MM-DD')}</p>;
+    } else if (count < 0) {
+        return <p>{count} days ago was {now.add(count, 'day').format('YYYY-MM-DD')}</p>;
+    } else {
+        return <p>Today is {now.format('YYYY-MM-DD')}</p>;
+    }
+}
+
+export default DisplayTime;

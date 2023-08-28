28bc2878b9472c185fda87cc6d0f6712d5fafc23
Curanda <103290697+Curanda@users.noreply.github.com>
2023-08-28 23:28:56 +0000

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './App.css'
+import StepCounter from "./assets/StepCounter.jsx";
+function App() {
+  return (
+    <>
+        <StepCounter />
+    </>
+  )
+}
+
+export default App

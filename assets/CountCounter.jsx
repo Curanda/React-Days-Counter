97815520cb599d1db72fbca64de3688d5d116f81
Curanda <103290697+Curanda@users.noreply.github.com>
2023-08-30 18:04:03 +0000

    Update CountCounter.jsx

--- assets/CountCounter.jsx
+++ assets/CountCounter.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react"
 import DisplayTime from "./DisplayTime.jsx";
 
-function CountCounter({ step }) {
+function CountCounter({ step, setStep }) {
     const [count, setCount] = useState(0)
 
     return (
     <>
         <div className={'counter-box'}>
             <button onClick={()=>setCount(c=>c-step)} style={{backgroundColor: 'white', color: 'black'}}>-</button>
-            <p>Count : {count}</p>
-            <button onClick={()=>setCount(c=>c+step)} style={{backgroundColor: 'white', color: 'black'}}>+</button>
+            <input onChange={(e)=>setCount(Number(e.target.value)+step)}/>
+            <button onClick={()=>setCount(c => c + step)} style={{backgroundColor: 'white', color: 'black'}}>+</button>
         </div>
-        <DisplayTime  count={count} />
+        <DisplayTime setCount={setCount} setStep={setStep} count={count} />
     </>
     )
 }

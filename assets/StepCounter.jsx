56635927545f419b55ebd950b6688d852f4c6398
Curanda <103290697+Curanda@users.noreply.github.com>
2023-08-30 18:04:32 +0000

    Update StepCounter.jsx

--- assets/StepCounter.jsx
+++ assets/StepCounter.jsx
@@ -5,16 +5,13 @@ import CountCounter from "./CountCounter.jsx";
 export default function StepCounter() {
     const [step, setStep] = useState(1)
 
-    return (
-        <>
-            <h1>Days Count</h1>
-            <div className={'counter-box'}>
-                <button onClick={()=>setStep(s=>s-1)} style={{backgroundColor: 'white', color: 'black'}}>-</button>
-                <p>Step : {step}</p>
-                <button onClick={()=>setStep(s=>s+1)} style={{backgroundColor: 'white', color: 'black'}}>+</button>
-            </div>
-            <CountCounter step={step} />
-        </>
-    )
+    return <>
+        <h1>Days Count</h1>
+        <div className={'counter-box'}>
+            <input type={'range'} min={0} max={10} value={step} onChange={(e)=>setStep(Number(e.target.value))}/>
+            <p>{step}</p>
+        </div>
+        <CountCounter step={step} setStep={setStep}/>
+    </>
 }
 

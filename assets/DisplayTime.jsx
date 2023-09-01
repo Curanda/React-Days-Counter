90b4169b744b760b93246b368f4cd62a4a9637cd
Curanda <103290697+Curanda@users.noreply.github.com>
2023-09-01 10:55:21 +0000

    Update DisplayTime.jsx

--- assets/DisplayTime.jsx
+++ assets/DisplayTime.jsx
@@ -8,9 +8,7 @@ function DisplayTime({ count }) {
         return <p>{count} days from today is {now.add(count, 'day').format('YYYY-MM-DD')}</p>
     } else if (count < 0) {
         return <p>{count} days ago was {now.add(count, 'day').format('YYYY-MM-DD')}</p>
-    } else {
-        return <p>Today is {now.format('YYYY-MM-DD')}</p>
-    }
+    } else return <p>Today is {now.format('YYYY-MM-DD')}</p>
 }
 
 export default DisplayTime;

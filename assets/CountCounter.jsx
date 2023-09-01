413cc5dd88bcfd61ab7362aab6549c54805bd001
Curanda <103290697+Curanda@users.noreply.github.com>
2023-09-01 10:54:02 +0000

    Update CountCounter.jsx

--- assets/CountCounter.jsx
+++ assets/CountCounter.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from "react"
+import React from "react"
 
 function CountCounter({ step, count, setCount }) {
 
-    return (
-        <div className={'counter-box'}>
-            <button onClick={()=>setCount(c=>c-step)} style={{backgroundColor: 'white', color: 'black'}}>-</button>
-            <input type='text' value={count} onChange={(e)=>setCount(Number(e.target.value)+step)}/>
-            <button onClick={()=>setCount(c => c + step)} style={{backgroundColor: 'white', color: 'black'}}>+</button>
-        </div>
-    )
+    return <div className={'counter-box'}>
+                <button onClick={()=>setCount(c=>c-step)} style={{backgroundColor: 'white', color: 'black'}}>-</button>
+                <input type='text' value={count} onChange={(e)=>setCount(Number(e.target.value)+step)}/>
+                <button onClick={()=>setCount(c => c + step)} style={{backgroundColor: 'white', color: 'black'}}>+</button>
+            </div>
 }
 
 export default CountCounter

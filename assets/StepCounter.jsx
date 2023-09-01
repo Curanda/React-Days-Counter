ff3c59f95ea706411b9d5951e93cb1054d911b3a
Curanda <103290697+Curanda@users.noreply.github.com>
2023-09-01 10:49:37 +0000

    Update StepCounter.jsx

--- assets/StepCounter.jsx
+++ assets/StepCounter.jsx
@@ -1,17 +1,13 @@
 import React, {useState} from "react"
-import CountCounter from "./CountCounter.jsx";
 
-
-export default function StepCounter() {
-    const [step, setStep] = useState(1)
+export default function StepCounter({ step, setStep }) {
 
     return <>
-        <h1>Days Count</h1>
+
         <div className={'counter-box'}>
             <input type={'range'} min={0} max={10} value={step} onChange={(e)=>setStep(Number(e.target.value))}/>
             <p>{step}</p>
         </div>
-        <CountCounter step={step} setStep={setStep}/>
     </>
 }
 

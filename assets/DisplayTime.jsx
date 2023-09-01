222a3ac9965255385d3c86b5507604c1d9bf7568
Curanda <103290697+Curanda@users.noreply.github.com>
2023-09-01 10:54:27 +0000

    Update DisplayTime.jsx

--- assets/DisplayTime.jsx
+++ assets/DisplayTime.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import dayjs from "dayjs";
 
-function DisplayTime({ count, setCount, setStep }) {
+function DisplayTime({ count }) {
     const now = dayjs();
 
     if (count > 0) {
-        return (
-            <>
-                <p>{count} days from today is {now.add(count, 'day').format('YYYY-MM-DD')}</p>
-            </>
-        );
+        return <p>{count} days from today is {now.add(count, 'day').format('YYYY-MM-DD')}</p>
     } else if (count < 0) {
-        return (
-            <>
-                <p>{count} days ago was {now.add(count, 'day').format('YYYY-MM-DD')}</p>;
-            </>
-        )
+        return <p>{count} days ago was {now.add(count, 'day').format('YYYY-MM-DD')}</p>
     } else {
-        return <p>Today is {now.format('YYYY-MM-DD')}</p>;
+        return <p>Today is {now.format('YYYY-MM-DD')}</p>
     }
 }
 

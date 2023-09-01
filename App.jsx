8d7faf9a755eba084ff615be681dd3c142b3f5ef
Curanda <103290697+Curanda@users.noreply.github.com>
2023-09-01 10:48:19 +0000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react'
 import './App.css'
 import StepCounter from "./assets/StepCounter.jsx";
+import CountCounter from "./assets/CountCounter.jsx";
+import DisplayTime from "./assets/DisplayTime.jsx";
+import Reset from "./assets/Reset.jsx";
 function App() {
-  return (
-    <>
-        <StepCounter />
-    </>
-  )
+    const [step, setStep] = useState(1)
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <h1>Days Count</h1>
+            <StepCounter step={step} setStep={setStep} />
+            <CountCounter step={step} setCount={setCount} count={count} />
+            <DisplayTime  count={count} setCount={setCount} />
+            {count !== 0 ? (<Reset setStep={setStep} setCount={setCount} />) : null}
+        </>
+    )
 }
 
 export default App
